5aeac4e7cbfc40149de7cf4ea11991c67dde55d3
Linus Ilbratt <118968403+LinusIlbratt@users.noreply.github.com>
2025-09-15 14:17:07 +0200

    adding db tables

--- db/migrations/V2__Add_products_table.sql
+++ db/migrations/V2__Add_products_table.sql
@@ -4,8 +4,3 @@ CREATE TABLE IF NOT EXISTS products (
     description TEXT,
     price DECIMAL(10, 2) NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS categories (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
