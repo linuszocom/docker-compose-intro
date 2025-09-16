ba4a25da24dbde82c2ad73e501a4b0481c32e451
Linus Ilbratt <118968403+LinusIlbratt@users.noreply.github.com>
2025-09-16 14:13:29 +0200

    migration

--- db/migrations/V1__create_tables.sql
+++ db/migrations/V1__create_tables.sql
@@ -2,3 +2,10 @@ CREATE TABLE IF NOT EXISTS messages (
     id SERIAL PRIMARY KEY,
     text VARCHAR(255)
 );
+
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL
+);
--- db/migrations/V2__add_products_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS products (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    stock INT NOT NULL
-);
